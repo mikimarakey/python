91ae38e1d405e2bcc26373993eb90ae4db1583a6
unknown <mikimarakey@gmail.com>
2024-02-18 08:58:56 +0300

    more courses

---
+++ Loop.py
@@ -0,0 +1,8 @@
+i=1
+while(i<6):
+    
+    print(i)
+    i+=1
+else:
+    print("it is 6")
+
---
+++ Operator.py
@@ -0,0 +1,13 @@
+# Arithemtic
+#num1=12
+#num2=23
+#num1==num2
+#num3=num1*num2 # num1,num2 and num3 ==> Operand
+#num1*=2 #num1= num1+2
+#print(num1==num2)
+
+# Logical operator and,or,not
+num1=20
+print(num1>0 and num1<21)
+name = "Mikias"
+print("M" in name)
