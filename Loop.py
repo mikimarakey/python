1d7e2e2a723560737fcdcfee499c25b1caa7fdf9
unknown <mikimarakey@gmail.com>
2024-02-18 09:23:33 +0300

    more courses

--- Loop.py
+++ Loop.py
@@ -1,8 +1,23 @@
-i=1
+
+"""i=1
 while(i<6):
     
     print(i)
     i+=1
 else:
-    print("it is 6")
+    print("it is 6")"""
+# student = ["Mikias", "Abis", "Jerry"]
+# for s in student:
+#     print(s)
 
+# name ="Mikias"
+# for n in name:
+#     print(n)    
+# for n in range(2,16,3):
+#     print(n)
+adj=["red","big","tasty"]
+fruits=["apple","banana","cherry"]
+for x in adj:
+    for b in fruits:
+        print(x,b)
+    
