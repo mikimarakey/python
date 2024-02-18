5d6e8413f21757a1e198a966aeecf54e3a1ba923
unknown <mikimarakey@gmail.com>
2024-02-18 09:34:37 +0300

    more courses

---
+++ function.py
@@ -0,0 +1,14 @@
+
+def sum(n1,n2):
+    return n1+n2
+def product(n1,n2):
+    return n1*n2
+def sub(n1,n2):
+    return n1-n2
+def div(n1,n2):
+    return n1/n2
+
+print(sum(15,15))   
+print(product(15,15))
+print(sub(15,15))
+print(div(15,15)) 
